bd89bd4b2178b662c68ddb1a363893cfedda9bd0
Joshua Baker <joshuakb2@gmail.com>
2022-10-21 14:12:40 -0600

    Use default loader

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ exports.replace = (options = {}) => {
         }
         const source = await fs.promises.readFile(args.path, "utf8");
         const contents = empty ? source : replaceCode(source, args.path, pattern, functionValues)
-        return { contents, loader: args.path.match(/tsx?$/) ? 'ts' : 'js' };
+        return { contents, loader: 'default' };
       });
     }
   };
